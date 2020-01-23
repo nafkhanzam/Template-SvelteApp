9479c653a0d626d8d5b97ef5937d17083e1850f7
nafkhanzam <nafkhanalzamzami@gmail.com>
2020-01-23 21:08:02 +0700

    first commit

---
+++ client/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Test from "./Test.svelte";
+	import Home from "./Home.svelte";
+	import NotFound from "./NotFound.svelte";
+	import Router from 'svelte-spa-router'
+
+	export let name;
+	
+	let routes = {
+		'/': Home,
+		// Exact path
+		'/test': Test,
+		'*': NotFound
+	}
+</script>
+<div>
+	<a href="#/test"><h1>Hello {name}!</h1></a>
+	<a href="#/"><h1>Hello HOME!</h1></a>
+	<a href="#/LMAO"><h1>Hello RANDOM!</h1></a>
+</div>
+<Router {routes}/>
